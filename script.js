c5bd0edd9deae3be174fe8047ffcc75ee58a05e6
Carjardying <carolinejardy.pro@gmail.com>
2025-07-17 15:09:45 +0200

    Step 2 : function to compare a given letter with the ones inside the object 'latinToMorse' and return the right value

--- script.js
+++ script.js
@@ -1,6 +1,57 @@
-function getLatinCharacterList (string){
-    console.log(string.split(""))
-    return string.split("")
+/** DICTIONNAIRE ANNEXE  1**/
+const latinToMorse = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+};
+
+/** CODE **/
+
+/**ETAPE  1**/
+function getLatinCharacterList(string) {
+  console.log(string.split(""));
+  return string.split("");
 }
 
 getLatinCharacterList("Hello WOrld");
+
+/**ETAPE 2 **/
+function translateLatinCharacter(character) {
+  // for ( A in objet latinToMorse: 
+  // 	if (character === latinToMorse.A){
+  // 		return  A.value
+  // 	}
+  // }
+  for (const letter in latinToMorse) {
+    if (character === letter) {
+      console.log(latinToMorse[letter]);
+	  return latinToMorse[letter]
+    }
+  }
+}
+
+translateLatinCharacter("A");
+translateLatinCharacter("B");
